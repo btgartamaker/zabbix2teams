470c89849de5538f118097ef79bbec68f6aa6ae0
bgartamaker <bgartamaker@digitalriver.com>
2020-08-05 14:49:15 -0500

    Modified to work better with zabbix mediatype

--- post2teams.py
+++ post2teams.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 from sys import argv
 import json
+import re
 import requests
 
 
@@ -7,9 +9,35 @@ class Zabbix2teams:
     def __init__(self, arguments):
         self.url = arguments[1]
         self.subject = arguments[2]
-        self.message = arguments[3].replace('"','\'').replace('\n','<br>')
-        self.eventurl = "https://"+arguments[4]+"/tr_events.php?triggerid="+arguments[5]+"&eventid="+arguments[6]
+        self.message = arguments[3]
+        self.eventurl = ""
         self.body = {}
+    
+    def parsebody(self):
+        zabbix2msteams = {
+            'dynamic': {
+                'zabbix_triger_id': '^Zabbix Trigger ID: .*',
+                'zabbix_domain': '^Zabbix Domain: .*',
+                'zabbix_event_id': '^Zabbix event ID: .*',
+            },
+            'static' : {
+
+            }
+        }
+
+        parsemessage = zabbix2msteams['static']
+        for key in zabbix2msteams['dynamic']:
+            items=re.findall(zabbix2msteams['dynamic'][key], self.message, re.MULTILINE)
+            if len(items) != 1:
+                parsemessage[key] = 'Problem with "%s" matching, found %i times' % (zabbix2msteams['dynamic'][key], len(items))
+                continue
+            else:
+                items[0] = items[0].split(':')[1].strip()
+                parsemessage[key] = items[0]
+        
+        self.message = self.message.replace('"','\'').replace('\n','<br>')
+        self.eventurl = "https://"+parsemessage['zabbix_domain']+"/tr_events.php?triggerid="+parsemessage['zabbix_triger_id']+"&eventid="+parsemessage['zabbix_event_id']
+
     
     def createbody(self):
         self.body = { 
@@ -38,5 +66,6 @@ class Zabbix2teams:
         requests.post(url = self.url, json = self.body )
 
 post2teams = Zabbix2teams(argv)
+post2teams.parsebody()
 post2teams.createbody()
 post2teams.send()
